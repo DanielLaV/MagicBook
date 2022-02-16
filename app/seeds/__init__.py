f0df28124781a315afcc3e53799fc85425b9e40b
DanielLaV <djjlav@gmail.com>
2022-02-15 22:42:18 -0600

    added comments seed data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .posts import seed_posts, undo_posts
+from .comments import seed_comments, undo_comments
 
 
 # Creates a seed group to hold our commands
@@ -12,6 +13,8 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_posts()
+    seed_comments()
     # Add other seed functions here
 
 
@@ -19,4 +22,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_posts()
+    undo_comments()
     # Add other undo functions here
