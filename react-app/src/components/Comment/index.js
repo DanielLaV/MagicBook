bfcc87b61468159526d3a1f7775b6704cf00e748
DanielLaV <djjlav@gmail.com>
2022-02-18 16:26:10 -0600

    started trying to display comment info

--- react-app/src/components/Comment/index.js
+++ react-app/src/components/Comment/index.js
@@ -1,11 +1,14 @@
-import './comment.css'
+import './comment.css';
+import { useSelector } from 'react-redux';
 
 
 function Comment({ comment }) {
-
+    // const commenter = useSelector(state => Object.values(state?.session).filter(comment => {
+    //     return comment.post_id === post.id;
+    // }));
 
     if (comment) {
-        console.log('====== inside COMMENT')
+        console.log('====== inside COMMENT', comment)
         return (
             <div className='commentWrapper'>
                 <div className='commentOwner'>
