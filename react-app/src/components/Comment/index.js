4fa7afbd7b6402bba68a5d80e7deda4f3a12eb4c
DanielLaV <djjlav@gmail.com>
2022-02-18 17:29:00 -0600

    edit and delete are rendering correctly on comments

--- react-app/src/components/Comment/index.js
+++ react-app/src/components/Comment/index.js
@@ -1,20 +1,31 @@
 import './comment.css';
+import DeleteCommentFormModal from '../DeleteCommentFormModal';
+import EditCommentFormModal from '../EditCommentFormModal';
 import { useSelector } from 'react-redux';
 
 
+
 function Comment({ comment }) {
-    const commenter = comment.user.username;
+    const commenter = comment.user;
+    const userId = useSelector(state => state?.session?.user?.id);
+    const showButtons = +userId === +commenter.id;
+
 
     if (comment) {
-        console.log('====== inside COMMENT', comment)
         return (
             <div className='commentWrapper'>
                 <div className='commentOwner'>
-                    {commenter}
+                    {commenter.username}
                 </div>
                 <div className="commentContent">
                     {comment.content}
                 </div>
+                {showButtons &&
+                    <div className='crudButtons'>
+                        <EditCommentFormModal comment={comment} />
+                        <DeleteCommentFormModal comment={comment} />
+                    </div>
+                }
 
             </div>
         )
