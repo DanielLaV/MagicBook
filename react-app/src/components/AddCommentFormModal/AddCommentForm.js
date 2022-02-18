4ddfe16a53d197ca2af522ec70c729f97f2b7c8d
DanielLaV <djjlav@gmail.com>
2022-02-17 18:41:13 -0600

    worked on addCommentForm

--- react-app/src/components/AddCommentFormModal/AddCommentForm.js
+++ react-app/src/components/AddCommentFormModal/AddCommentForm.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as commentActions from "../../store/comments";
 import { useParams } from "react-router-dom";
 import './AddCommentForm.css';
 
 function AddCommentForm({ payload }) {
   const dispatch = useDispatch();
+  const user_id = useSelector(state => state.session.user.id);
   const { postId } = useParams();
   const setShowModal = payload
-  const [front, setFront] = useState("");
-  const [back, setBack] = useState("");
+  const [content, setContent] = useState("");
   const [errors, setErrors] = useState([]);
   const [success, setSuccess] = useState("");
+
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setErrors([]);
     const payload = {
-      front,
-      back,
+      content,
+      user_id,
       post_id: +postId
     }
     return dispatch(commentActions.createComment(payload))
@@ -29,10 +30,10 @@ function AddCommentForm({ payload }) {
             setErrors(response.errors)
             return
           }
-          setSuccess("Success!");
+          setSuccess("Be kind in your comments!");
           setTimeout(() => {
             setShowModal(false);
-          }, 1500);
+          }, 850);
         }
       );
   };
@@ -40,23 +41,11 @@ function AddCommentForm({ payload }) {
   return (
 
     <div className='formView'>
-      <div className="preview">
 
-    <div className="form-container">
-      <h2 style={{color:"green"}}>
-        {success}
-      </h2>
-        <h2 className="preview-title">Preview</h2>
-        <p className="side">Front:</p>
-        <div>
-
-        <div className="preview-text"><div className="texter">{front}</div></div>
-        </div>
-        <p className="side">Back:</p>
-        <div>
-
-        <div className="preview-text"><div className="texter">{back}</div></div>
-        </div>
+      <div className="form-container">
+        <h2 style={{ color: "green" }}>
+          {success}
+        </h2>
       </div>
       <ul className="error-list">
         {errors.map((error, idx) => (
@@ -66,21 +55,11 @@ function AddCommentForm({ payload }) {
       <form className='form' onSubmit={handleSubmit}>
         <div className="text-container">
           <textarea
-            id="front"
             type="text"
-            value={front}
-            onChange={(e) => setFront(e.target.value)}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
             required
-            placeholder="Enter what you want to appear on the front of the comment. The front can be something like a question, concept, or vocabulary word."
-            className="input"
-          />
-          <textarea
-            id="back"
-            type="text"
-            value={back}
-            onChange={(e) => setBack(e.target.value)}
-            required
-            placeholder="Enter what you want to appear on the back of the comment. This would be the answer or response to the prompt on the front of the comment."
+            placeholder="Write a comment..."
             className="input"
           />
         </div>
@@ -93,16 +72,14 @@ function AddCommentForm({ payload }) {
           <button
             type="button"
             className="form-button"
-            onClick={(e) => {
+            onClick={() => {
               setShowModal(false);
-              setFront("");
-              setBack("");
+              setContent("");
             }}>
             Cancel
           </button>
         </div>
       </form>
-    </div>
     </div>
   );
 }
