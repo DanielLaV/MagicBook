05d94b68eedcbaee4075eb7cae884573b01aa030
DanielLaV <djjlav@gmail.com>
2022-02-21 17:46:45 -0600

    splash page about half done

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../store/session";
 import splash from '../../assets/splash.jpeg';
 import './SplashPage.css'
+import LoginForm from '../LoginForm';
+import SignupForm from '../SignupForm';
 
 
 const SplashPage = () => {
-    let sessionLinks;
 
     const dispatch = useDispatch();
+    const [form, setForm] = useState('Login');
+    let showLogin = form === 'Login';
 
     const demoLogin = () => {
         const email = 'chosenOne@hogwarts.edu';
@@ -16,20 +19,34 @@ const SplashPage = () => {
         return dispatch(login(email, password));
     }
 
-    sessionLinks = (
-        <div>
-            <div className="bgImageContainer">
-                <img className="bgImage" src={splash} alt="potion bottle with wand"></img>
-            </div>
-            <div className="userAuthContainer">
 
-            </div>
-        </div>
-    )
+    const clickLogin = () => {
+        setForm('Login')
+    }
+
+    const clickSignup = () => {
+        setForm('Signup')
+    }
 
     return (
         <div className="splashPage">
-            {sessionLinks}
+            <div className="bgImageContainer">
+                <img className="bgImage" src={splash} alt="potion bottle with wand"></img>
+            </div>
+            <div className="rightHalf">
+                <div className="userAuthContainer">
+                    <div className="userAuthHeader">
+                        <button className="authButton" onClick={clickLogin}>Log In</button>
+                        <button className="authButton" onClick={clickSignup}>Sign Up</button>
+                    </div>
+                    <div className="userAuth">
+                        <div className="currForm">
+                            {showLogin && <LoginForm />}
+                            {!showLogin && <SignupForm />}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
