dcefc829e931523b5331334ca50cdc94ad4559d6
DanielLaV <djjlav@gmail.com>
2022-02-24 11:57:50 -0600

    changed the origins url in socketio.py

--- app/socketio.py
+++ app/socketio.py
@@ -5,8 +5,8 @@ import os
 # configure cors_allowed_origins
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
-        'http://actual-app-url.herokuapp.com',
-        'https://actual-app-url.herokuapp.com'
+        'http://magicbook-hp.herokuapp.com',
+        'http://magicbook-hp.herokuapp.com'
     ]
 else:
     origins = "*"
