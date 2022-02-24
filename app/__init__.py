57fd9ff83c122f0b9d6b3de4cf2075148bbf85d3
DanielLaV <djjlav@gmail.com>
2022-02-23 22:38:34 -0600

    added chat room

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import generate_csrf
 from flask_login import LoginManager
+from .socketio import socketio
 
 from .models import db, User
 from .api import user_routes, auth_routes, post_routes, comment_routes
@@ -35,6 +36,7 @@ app.register_blueprint(post_routes, url_prefix='/api/posts')
 app.register_blueprint(comment_routes, url_prefix='/api/comments')
 db.init_app(app)
 Migrate(app, db)
+socketio.init_app(app)
 
 # Application Security
 CORS(app)
@@ -72,3 +74,7 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ react-app/src/components/Chat/index.js
@@ -0,0 +1,62 @@
+import { io } from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import './chat.css';
+
+let socket;
+
+const Chat = () => {
+
+    const [messages, setMessages] = useState([]);
+    const [chatInput, setChatInput] = useState('');
+    const user = useSelector(state => state.session.user)
+
+    useEffect(() => {
+        socket = io();
+
+        socket.on('chat', (chat) => {
+            setMessages(messages => [...messages, chat])
+        })
+
+        return (() => {
+            socket.disconnect()
+        })
+    }, []);
+
+    function scrollToBottom() {
+        const element = document.getElementById("chatMessages");
+        element.scrollTop = element.scrollHeight;
+    }
+
+    const sendChat = (e) => {
+        console.log('in send chat')
+        e.preventDefault();
+        socket.emit("chat", { user: user.username, msg: chatInput });
+        setChatInput('');
+        scrollToBottom();
+    }
+
+    return (
+        <div className='chatPage'>
+            <div className='chatContainer'>
+                <div id='chatMessages'>
+                    {messages.map((message, ind) => (
+                        <div className='eachChatMessage' key={ind}>{`${message.user}: ${message.msg}`}</div>
+                    ))}
+                </div>
+                <form onSubmit={sendChat} className='chatForm'>
+                    <input
+                        value={chatInput}
+                        className='chatBox'
+                        required
+                        placeholder='Send a chat message'
+                        onChange={e => setChatInput(e.target.value)}
+                    />
+                    <button hidden type="submit">Send</button>
+                </form>
+            </div>
+        </div>
+    )
+};
+
+export default Chat;
